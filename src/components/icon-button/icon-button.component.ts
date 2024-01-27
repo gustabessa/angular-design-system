5eb2b3d28dc6a85e31834a43cf7b2a3e79054b48
Gustavo Bessa <gustavoux@yahoo.com.br>
2024-01-27 18:18:25 -0300

    feat: create icon button to be used on header

---
+++ src/components/icon-button/icon-button.component.ts
@@ -0,0 +1,40 @@
+import { Component, input, computed } from '@angular/core';
+
+type ColorTypes =
+  | number
+  | 'primary'
+  | 'secondary'
+  | 'contrast'
+  | 'contrast-100'
+  | 'contrast-200'
+  | 'contrast-300';
+
+@Component({
+  standalone: true,
+  selector: 'ds-icon-button',
+  templateUrl: './icon-button.component.html',
+  styleUrls: ['./icon-button.component.scss'],
+})
+export class IconButtonComponent {
+  icon = input.required<string>();
+
+  sizePx = input.required<number>();
+
+  color = input<ColorTypes>();
+
+  colorValue = computed(() => {
+    if (Number.isNaN(Number(this.color()))) {
+      return `var(--${this.color()})`;
+    }
+    return this.color();
+  });
+
+  inlineStyle = computed(
+    () => `-webkit-mask: url(${this.icon()}) no-repeat 50% 50%;
+            mask: url(${this.icon()}) no-repeat 50% 50%;
+            mask-size: cover;
+            background-color: ${this.colorValue()};
+            height: ${this.sizePx()}px;
+            width: ${this.sizePx()}px`,
+  );
+}
