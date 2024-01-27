fb12c81fccc4d09bf505403f56b2118ecd1811bd
Gustavo Bessa <gustavoux@yahoo.com.br>
2024-01-27 18:21:21 -0300

    feat: create layout component with content projection

--- src/feature/layout/layout.component.ts
+++ src/feature/layout/layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '@components';
+import { IconButtonComponent } from '../../components/icon-button/icon-button.component';
 
 @Component({
   selector: 'ds-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, IconButtonComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
