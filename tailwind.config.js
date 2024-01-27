8e232117eafe651bbd4e1ad943f8636501bb7665
Gustavo Bessa <gustavoux@yahoo.com.br>
2024-01-27 18:20:29 -0300

    chore: configure tailwind custom theme variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       colors: {
-        primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
-        buttons: 'var(--color-buttons)',
-        typography: 'var(--color-typography)',
+        primary: 'var(--primary)',
+        contrast: {
+          DEFAULT: 'var(--contrast)',
+          100: 'var(--contrast-100)',
+          200: 'var(--contrast-200)',
+          300: 'var(--contrast-300)',
+          400: 'var(--contrast-400)',
+        },
+        secondary: 'var(--secondary)',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
       },
     },
   },
