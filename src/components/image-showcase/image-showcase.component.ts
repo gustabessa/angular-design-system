1dd790bd53ca493e32e17aee7d686aa5783de303
Gustavo Bessa <gustavoux@yahoo.com.br>
2024-01-30 00:41:01 -0300

    lint: remove unused promise reject and imports

--- src/components/image-showcase/image-showcase.component.ts
+++ src/components/image-showcase/image-showcase.component.ts
@@ -17,7 +17,7 @@ export class ImageShowcaseComponent {
       .subscribe(async (images) => {
         const loadImagesPromises = images.map(
           (image) =>
-            new Promise<void>((resolve, reject) => {
+            new Promise<void>((resolve) => {
               const imgElement = new Image();
               imgElement.src = image.src;
               imgElement.onload = () => resolve();
